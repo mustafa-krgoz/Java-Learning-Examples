abf048a654a28f7245953986ead2049235f51c5c
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2022-09-20 10:48:00 +0300

    Add files via upload

---
+++ OgrenciTest.java
@@ -0,0 +1,12 @@
+
+public class OgrenciTest {
+
+	public static void main(String[] args) {
+		Ogrenci nesne1 = new Ogrenci(221, "Mustafa Karagöz", 1000f);
+		Ogrenci nesne2 = new Ogrenci(1071, "Mami", 1500f);
+		
+		nesne1.bilgileriGoster();
+		nesne2.bilgileriGoster();
+	}
+
+}
