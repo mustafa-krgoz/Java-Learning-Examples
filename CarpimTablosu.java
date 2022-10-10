56fcbe7e8faa2432212f34033b2aa33840e57210
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2022-10-10 23:30:40 +0300

    Add files via upload

---
+++ CarpimTablosu.java
@@ -0,0 +1,24 @@
+
+public class CarpimTablosu {
+
+	public static void main(String[] args) {
+		// Tablonun baþlýðý
+		System.out.println("               Çarpým Tablosu");
+		
+		// Sayý baþlýðý
+		System.out.print("    ");
+		for(int j = 1 ; j<=9 ; j++)
+			System.out.print( "   " + j);
+		System.out.println("\n-------------------------------------------------");
+		
+		//tablonun gövdesi
+		for(int i =1 ; i<=9 ; i++) {
+			System.out.print(i + " | ");
+		for(int j = 1 ; j<=9 ; j++) 
+			System.out.printf("%4d", i*j);
+		
+		System.out.println();
+		}
+	}
+
+}
